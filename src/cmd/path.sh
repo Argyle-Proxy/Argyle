3d14ed937dee49d97c74b39ac92110fb516e64ca
Quartinal <prehistorical.dinosaurs@outlook.com>
2023-12-15 09:50:28 -0800

    Updates :\

--- src/cmd/path.sh
+++ src/cmd/path.sh
@@ -3,8 +3,6 @@
 if [ ! -f ".run-once-marker" ]; then
   echo "Running the command for the first time..."
 
-  sudo chown -R $USER /home/Argyle-Proxy/Argyle/src/cmd/path.sh
-
   export PATH="$PATH:/opt/build/repo/src/src/node_modules/.bin"
 
   touch .run-once-marker
