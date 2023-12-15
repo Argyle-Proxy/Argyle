8f80f156a8eaf921c28490da9d2a5307cf26e518
Quartinal <prehistorical.dinosaurs@outlook.com>
2023-12-15 12:26:02 -0800

    Netlify pretty please work

--- src/cmd/path.sh
+++ src/cmd/path.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/bash
 
-if [ ! -f ".run-once-marker" ]; then
-  echo "Running the command for the first time..."
-
-  export PATH="$PATH:/opt/build/repo/src/src/node_modules/.bin"
-
-  touch .run-once-marker
-else
-  echo "Command already executed. Skipping..."
-fi
+export PATH="$PATH:/opt/build/repo/src/node_modules/.bin"
