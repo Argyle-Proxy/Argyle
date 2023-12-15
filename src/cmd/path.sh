9686278ac791d7e52d625d560b7f6efa6af4af6a
Quartinal <prehistorical.dinosaurs@outlook.com>
2023-12-15 09:54:59 -0800

    Again p a t h . s h

--- src/cmd/path.sh
+++ src/cmd/path.sh
@@ -2,9 +2,7 @@
 
 if [ ! -f ".run-once-marker" ]; then
   echo "Running the command for the first time..."
-
-  sudo chown -R $USER /opt/build/repo/src/cmd/path.sh
-
+  
   export PATH="$PATH:/opt/build/repo/src/src/node_modules/.bin"
 
   touch .run-once-marker
