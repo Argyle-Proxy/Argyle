1f6f89f0bb4eeb9a496463c6c4296522ef717a56
Quartinal <prehistorical.dinosaurs@outlook.com>
2023-12-15 10:31:12 -0800

    Again ... MUAHAHA Vercel

--- src/cmd/path.sh
+++ src/cmd/path.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f ".run-once-marker" ]; then
   echo "Running the command for the first time..."
-  
+
   export PATH="$PATH:/opt/build/repo/src/src/node_modules/.bin"
 
   touch .run-once-marker
