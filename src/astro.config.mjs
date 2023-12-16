dae6a1f52ff7a3d889873b4815af8721ab489017
Quartinal <prehistorical.dinosaurs@outlook.com>
2023-12-16 07:31:16 -0800

    I'm doing my part 🙏

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import netlify from "@astrojs/netlify/functions";
-import { vercel } from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   output: "src/pages",
   adapter: [
     netlify(),
-    vercel()
   ],
   server: {
     headers: {
