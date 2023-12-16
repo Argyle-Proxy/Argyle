bb614356b4d927ceae4d90126ce54e4af2a37837
Quartinal <prehistorical.dinosaurs@outlook.com>
2023-12-16 07:18:27 -0800

    Add Vercel adapter to astro.c o n f i g.mjs

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   output: "src/pages",
-  adapter: netlify(),
+  adapter: [
+    netlify(),
+    vercel()
+  ],
   server: {
     headers: {
       'Access-Control-Allow-Origin': '*'
