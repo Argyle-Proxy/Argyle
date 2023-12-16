5f64ba5a6593fed9402d8cd73556d12424ba0a8e
Quartinal <prehistorical.dinosaurs@outlook.com>
2023-12-16 09:44:44 -0800

    Remove array from astro.config.mjs 😵‍💫

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -7,9 +7,7 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   output: "src/pages",
-  adapter: [
-    netlify(),
-  ],
+  adapter: netlify(),
   server: {
     headers: {
       'Access-Control-Allow-Origin': '*'
